84804fc14ca9755e1847540522eff683ae224b52
TiltonLAW <128943089+TiltonLAW@users.noreply.github.com>
2023-05-20 06:07:34 -0400

    Add files via upload
    Improvements on query prompt

--- search.py
+++ search.py
@@ -4,8 +4,8 @@ import requests
 
 # search.py
 
-os.environ['OPENAI_API_KEY'] = 'insert-your-openai-api-here'
-os.environ['CASELAW_API_KEY'] = 'insert-your-case.lew-api-here'
+os.environ['OPENAI_API_KEY'] = '[insert your openai api here]'
+os.environ['CASELAW_API_KEY'] = '[inser your case.law api here]'
 
 # create good search terms from the prompt
 
@@ -14,8 +14,8 @@ def generate_query(prompt):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"Name one, and only one, fitting legal doctrine that addresses this question: \"{prompt}\"? If no legal doctrine fits, please reduce the question to the three most important words to get helpful results from a caselaw search engine. Do not answer in the narrative form, only provide the search terms in your reply and nothing else:"},
+            {"role": "system", "content": "You are a helpful legal, research assistant."},
+            {"role": "user", "content": f"Create a search query for this question: \"{prompt}\"? Please reduce the question to the three most important words to get helpful results from a caselaw search engine. Do not answer in the narrative form, only provide the search terms in your reply and nothing else.  For example, a question 'can a person use force in defense of their own personal proerty?' should return a result: 'force defense property'; and the question, 'what are the elements of promissory estoppel?' should return only: 'elements promissory estoppel.'"},
         ],
         temperature=0.8,
         max_tokens=10,
@@ -34,7 +34,7 @@ def get_search_results(query):
     query_encoded = requests.utils.quote(query)
     print(f"Query: '{query}'")
     response = requests.get(
-        f'https://api.case.law/v1/cases/?page_size=5&search={query_encoded}&jurisdiction=vt&court=vt&decision_date_min=1970',
+        f'https://api.case.law/v1/cases/?page_size=4&search={query_encoded}&jurisdiction=vt&court=vt&decision_date_min=1970',
         headers={'Authorization': f'Token {case_law_api_key}'}
     )
     search_results = response.json()
@@ -44,4 +44,3 @@ def get_search_results(query):
         print(f"Result {i + 1}: {result['name']}")    
 
     return search_results
-
