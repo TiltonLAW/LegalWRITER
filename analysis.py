cd8478b6537209548120ed39b9ad997e767470b0
TiltonLAW <128943089+TiltonLAW@users.noreply.github.com>
2023-05-20 06:09:00 -0400

    Add files via upload
    new methodology for sorting relevance and creating final output - only moderate improvement - still needs works

--- analysis.py
+++ analysis.py
@@ -1,88 +1,71 @@
-# analysis.py
-
 import os
-import openai
-import re
-import requests
+import json
 
-def get_relevance_scores(prompt, parentheticals):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+def is_narrative(parenthetical):
+    return len(parenthetical.split()) > 7
 
-    # Check if there are any parentheticals
-    if not parentheticals:
-        return []
-
-    # Join all parentheticals into a single string, each separated by a newline
-    all_parentheticals = "\n".join(f"Case text: \"{p}\"." for p in parentheticals)
-
-    print(f"Sending the following texts for scoring:\n{all_parentheticals}")  # Debug print
-
-    response = openai.Completion.create(
-        model="text-curie-001",
-        prompt=f"On a scale of 1 to 10, how relevant is the statement '{all_parentheticals}' to the prompt: \"{prompt}\".\n\n Score with a number only, do not add any narrative or words.  Relevance score: ",
-        max_tokens=20,
-        temperature=0.8,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-
-    # Split the response text into individual scores
-    scores_text = response.choices[0].text.strip().split("\n")
-    print(f"Received scores: {scores_text}")  # Debug print
-
-    # Extract only the score (digit) from each score text
-    scores = []
-    for score_text in scores_text:
-        match = re.search(r"\d+", score_text)  # Use regex to find the first number in the string
-        if match:
-            score = int(match.group())  # Convert the matched number to an int
-            scores.append(score)
-        else:
-            scores.append(0)  # If no number is found, append 0
-
-    print(f"Scores: {scores}")  # Debug print scores
-    return scores
+def parse_json(json_file):
+    narrative_parentheticals = []
+    
+    with open(json_file) as file:
+        data = json.load(file)
+        for citation in data.get('cites_to', []):
+            for pin_cite in citation.get('pin_cites', []):
+                parenthetical = pin_cite.get('parenthetical', '')
+                if is_narrative(parenthetical):
+                    narrative_parentheticals.append(parenthetical)
+                    
+    return narrative_parentheticals[:5]  # Only take the first 5
 
 def filter_and_rank_cases(prompt, search_results):
+    case_infos = []
+    research_body = []
+
     if 'results' not in search_results:
-        return [], []
+        return research_body, case_infos
 
     results = search_results['results'][:5]  
 
-    case_infos = []
-
-    # Calculate relevance scores for each case
+    # Process each case
     for case in results:
-        print(f"Processing case: {case['name']}")  # Debug print
+        print(f"Processing case: {case['name']}\n\n")  # Debug print
 
         cites_to = case.get('cites_to', [])
+        case_parentheticals = []
         for cite in cites_to:
             pin_cites = cite.get('pin_cites', [])
-            # Filter out parentheticals with less than 5 words
-            parentheticals = [pin_cite.get('parenthetical') for pin_cite in pin_cites if pin_cite.get('parenthetical') and len(pin_cite.get('parenthetical').split()) > 5]
-
-            print(f"Filtered parentheticals (more than 5 words): {parentheticals}")  # Debug print
-
+            # Filter out parentheticals that aren't narrative
+            parentheticals = [pin_cite.get('parenthetical') for pin_cite in pin_cites if pin_cite.get('parenthetical') and is_narrative(pin_cite.get('parenthetical'))]
             if parentheticals:
-                scores = get_relevance_scores(prompt, parentheticals[:4])  # Limit to the first 4 parentheticals
+                print(f"Filtered parentheticals : {parentheticals}\n\n")  # Debug print
+                case_parentheticals.extend(parentheticals)
 
-                for parenthetical, score in zip(parentheticals[:4], scores):  # Only consider first 4 parentheticals
-                    print(f"Relevance score for '{parenthetical}': {score}")  # Debug print
+        # Only take the first 5 parentheticals
+        case_parentheticals = case_parentheticals[:7]
 
-                    # Consider a parenthetical helpful only if the score is between 7 and 10 (inclusive)
-                    if 7 <= score <= 10:  
-                        # Get the official citation
-                        citations = case.get('citations', [])
-                        official_cite = next((c['cite'] for c in citations if c['type'] == 'official'), '')
-                        
-                        # Format the citation as 'Name, Citation (Year)'
-                        formatted_citation = f"{case['name_abbreviation']}, {official_cite} ({case['decision_date'][:4]})" if official_cite else ''
+        # Get the official citation
+        citations = case.get('citations', [])
+        official_cite = next((c['cite'] for c in citations if c['type'] == 'official'), '')
 
-                        case_infos.append({
-                        'case_name': case['name_abbreviation'],
-                        'citation': formatted_citation,
-                        'helpful_parenthetical': parenthetical
-                    })
+        # Format the citation as 'Name, Citation (Year)'
+        formatted_citation = f"{case['name_abbreviation']}, {official_cite} ({case['decision_date'][:4]})" if official_cite else ''
 
-    return results, case_infos
+        # Add the slim case text to the research_body
+        slim_case_text = {"case_name": case['name'], 
+                          "citation": formatted_citation, 
+                          "parentheticals": case_parentheticals}
+        research_body.append(slim_case_text)
+
+    # Iterate over the research body
+    for slim_case_text in research_body:
+        # Create a case information dictionary
+        case_info = {
+            "case_name": slim_case_text['case_name'],
+            "citation": slim_case_text['citation'],
+            "helpful_parenthetical": ', '.join(slim_case_text['parentheticals']),
+        }
+    
+        # Add the case information dictionary to the list
+        case_infos.append(case_info)
+
+    return research_body, case_infos
--- output.py
+++ output.py
@@ -13,10 +13,10 @@ def generate_response(prompt, relevant_cases, case_infos):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful, legal assistant."},
-            {"role": "user", "content": f"Based on the research ({case_info}) only, answer the legal question(\"{prompt}\").\nDisregard all off topic and irrelevant portions of the research. \nProviding citations and relevant quotes, if any, provide an comprehesive and accurate legal analysis answering the question.\nAt the end of the answer, add 'Cases researched:' then list names and citations of all relevant cases found in the research."},
+            {"role": "user", "content": f"Please answer the legal question(\"{prompt}\") as if you are a legal assistant helping a lawyer write a motion.  If any information in \"{case_info}\" is helpful please use it in you answer. Please explain what the law about the topic, provide citations and relevant quotes in this section.  Disregard any off topic and irrelevant portions of the \"{case_info}\". \nFinally, summarize the conclusion in a straighforward legal statement of the law.\nAt the end of the answer, add 'Cases researched:' then list names and citations of all relevant cases found in the research."},
         ],
         temperature=0.5,
-        max_tokens=2420,
+        max_tokens=2096,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
